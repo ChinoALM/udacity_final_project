fd6627f4f93c75c0378ca70ec568c7bf868a6718
ChinoALM <mortona35@gmail.com>
2017-12-01 23:56:56 -0500

    the beginnings of my final project

---
+++ js/resumeBuilder.js
@@ -0,0 +1,16 @@
+var bio = {
+    "name" : "Anthony L Morton",
+    "role" : "Mobile iOS Developer",
+    "contacts": {
+        "mobile": "317-666-6666",
+        "email": "mortona35@gmail.com",
+        "github": "ChinoALM",
+        "twitter": "@CurlyMorton",
+        "location": "Indianpolois"
+    },
+    "welcomeMessage": "Seize the day! What have you done today to better your tomorrow?",
+    "skills": ["awesomeness", "delivering things", "serving tables", "domestic chores"],
+    "bioPic": "images/resume.jpg"
+}
+
+var education =
