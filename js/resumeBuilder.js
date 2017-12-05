f2b57394fffeb5d58a259de12ac90ba76991e885
ChinoALM <mortona35@gmail.com>
2017-12-04 21:30:58 -0500

    var bio, education, work with self.display

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,160 @@
 var bio = {
-    "name" : "Anthony L Morton",
-    "role" : "Mobile iOS Developer",
+    "name": "Anthony L Morton",
+    "role": "Front-end Developer",
     "contacts": {
-        "mobile": "317-666-6666",
+        "mobile": "555-5555",
         "email": "mortona35@gmail.com",
-        "github": "ChinoALM",
-        "twitter": "@CurlyMorton",
-        "location": "Indianpolois"
+        "github": "@ChinoALM",
+        "location": "Indianapolis, IN, US"
     },
-    "welcomeMessage": "Seize the day! What have you done today to better your tomorrow?",
-    "skills": ["awesomeness", "delivering things", "serving tables", "domestic chores"],
-    "bioPic": "images/resume.jpg"
+    "welcomeMessage": "What have you done today to better your tomorrow? Seize the day!",
+    "skills": [
+        "SQL", "HTML", "CSS", "Java Script"],
+    "biopic": "images/resume.jpg"
 }
 
-var education =
+var education = {
+    "schools": [
+        {
+            "name": "Udacity",
+            "degree": "Nano Degree",
+            "majors": ["Intro to Programming"],
+            "dates": "March 2017-December 2017",
+            "location": "Online",
+            "url": "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+        },
+        {
+            "name": "Wabash College",
+            "location": "Crawfordsville, IN",
+            "degree": "Bachelors of the Arts",
+            "majors": ["Theatre"],
+            "dates": "2006-2010",
+            "url": "http://www.wabash.edu/"
+        },
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Mad Hat Enterprise",
+            "title": "Character Performer & Entertainment Specialist",
+            "dates": "April 2017 - Present",
+            "location" : "Indianapolis, IN, US",
+            "description": "Inspires imagination and creativity as a character " +
+                "performer in the role of the Mad Hatter, attending music " +
+                "festivals and music related events across the United States. " +
+                "Production includes an accent; a custom, personalized costume; " +
+                "full make-up, and gloving (performance art). Volunteer performances " +
+                "in 2017 were on display in Atlantic City, NJ; Rothbury, MI; " +
+                "Broomfield, CO; and Hampton, VA."
+        },
+        {
+            "employer": "Iozzo's Garden of Italy",
+            "title": "Hospitality Specialist",
+            "dates": "February 2015 - Present",
+            "location" : "Indianapolis, IN, US",
+            "description": "Provides primary front of house support for " +
+                "Iozzo's Garden of Italy. Executes deployments of menu knowledge " +
+                "requirments, managing client expectations, communicating with " +
+                "internal departments to achieve standards of success everyday."
+        },
+        {
+            "employer": "McCormick & Schmicks",
+            "title": "Hospitality Specialist",
+            "dates": "February 2014 - February 2015",
+            "location" : "Indianapolis, IN, US",
+            "description": "Provided primary front of house support for " +
+                "McCormick & Schmicks. Executed deployments of menu knowledge " +
+                "requirments, managing client expectations, communicating with " +
+                "internal departments to achieve standards of success everyday."
+        },
+        {
+            "employer": "Back to the Roots",
+            "title": "Hospitality Specialist",
+            "dates": "January 2013 - September 2013",
+            "location" : "Chelsea, MI, US",
+            "description": "Provided primary front of house support for " +
+                "Back to the Roots. Executed deployments of menu knowledge " +
+                "requirments, managing client expectations, communicating with " +
+                "internal departments to achieve standards of success everyday."
+        }
+    ]
+}
+
+bio.display= function() {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").append(formattedName);
+
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").append(formattedRole);
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#header, #footer-entry" ).append(formattedMobile);
+
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#header, #footer-entry").append(formattedEmail);
+
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#header, #footer-entry").append(formattedGithub);
+
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#header, #footer-entry").append(formattedLocation);
+
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedWelcomeMsg);
+
+    var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(formattedPic);
+
+    for(var i=0; i<bio.skills.length; i+=1){
+        var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
+        $("#header").append(formattedSkills);
+    }
+}
+
+work.display = function() {
+    for (job in work.jobs){
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedWorkLocation);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
+
+education.display = function() {
+    for(school in education.schools) {
+        $("#education").append(HTMLschoolStart);
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedSchoolTitle = formattedSchoolName + formattedDegree;
+
+        $(".education-entry:last").append(formattedSchoolTitle);
+
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+        $(".education-entry:last").append(formattedSchoolDates);
+
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        $(".education-entry:last").append(formattedSchoolLocation);
+
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        $(".education-entry:last").append(formattedMajor);
+    }
+}
+
+bio.display()
+work.display()
+education.display()
